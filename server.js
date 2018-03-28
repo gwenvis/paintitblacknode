d4dea99eee9378d66b632df178aeff1ce4be27b4
Antonio Bottelier <Antonio3338@gmail.com>
2018-03-28 11:03:59 +0200

    asfohajfhasf

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 let drawing = [];
 
@@ -29,6 +31,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
     console.log('listening on port 3000');
 });
